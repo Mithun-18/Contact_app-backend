f0414d5c5bbd767332fb694bc7d503ec71345efc
Mithun-18 <mithunpoojary180@gmail.com>
2025-03-12 14:46:25 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (_, res) => res.send("server is running...!"));
+
+export { app };
